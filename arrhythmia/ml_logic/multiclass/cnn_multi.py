00a25d221886b3233c7d5b550baa02d4dd37b6c5
lucasfrance <1lucas.france@gmail.com>
2024-11-29 15:42:26 +0100

    package CNN multi drop F

--- arrhythmia/ml_logic/multiclass/cnn_multi.py
+++ arrhythmia/ml_logic/multiclass/cnn_multi.py
@@ -109,3 +109,12 @@ def apply_cnn(filename):
 
     res = model.evaluate(X_test, y_test, verbose = 1 )
     print(res)
+
+
+if __name__=="__main__":
+ #   apply_cnn(../../../)
+    rootpath=(os.path.dirname(__file__)) #print(os.getcwd())
+    relativepath="../../../raw_data/MIT-BIH_dropF.csv"
+    csv_path=os.path.join(rootpath,relativepath)
+    print(csv_path)
+    print(apply_cnn(csv_path))
