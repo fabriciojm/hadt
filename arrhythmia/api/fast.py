8fed955426dd54d12c56a0dab2b1beeaf1c9b651
lucasfrance <1lucas.france@gmail.com>
2024-12-02 14:50:35 +0100

    ping pong

--- arrhythmia/api/fast.py
+++ arrhythmia/api/fast.py
@@ -1,18 +1,16 @@
+from fastapi import FastAPI
+
+
 
 app = FastAPI()
-app.state.model = #MODEL
-
-@app.get("/predict")
-# Let's use our model
-app.state.model.predict()
-
-
-
-
-
-
-
 
 @app.get("/")
 def root():
     return dict(greeting="Hello")
+
+
+# app.state.model = #MODEL
+
+# @app.get("/predict")
+# # Let's use our model
+# app.state.model.predict()
