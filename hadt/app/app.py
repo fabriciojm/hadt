ba3ccdfcf204e62ce86e2ded509ae06b8506e1d8
Fabricio Jimenez Morales <fabricioajm@gmail.com>
2025-01-13 14:58:19 +0100

    first version of new webapp

--- hadt/app/app.py
+++ hadt/app/app.py
@@ -1,46 +1,36 @@
 import streamlit as st
-import pandas as pd
 import requests
-import numpy as np
-import time
+import pandas as pd
 
-st.set_page_config(
-    page_title="Arrhythmia",
-    page_icon="❤️",
-    layout="centered",
-    initial_sidebar_state="auto")
+st.title("Arrhythmia Detection")
 
-'''
-# ❤️🫀🥼
-'''
+models = {"CNN Binary": "cnn_binary_model.h5",
+          "LSTM Binary": "lstm_binary_model.h5",
+          "PCA XGBoost Binary": "pca_xgboost_binary_model.pkl",
+          "CNN Multi": "cnn_multi_model.h5",
+          "LSTM Multi": "lstm_multi_model.h5",
+          "PCA XGBoost Multi": "pca_xgboost_multi_model.pkl"}
 
-st.markdown('''
-Howdy, dear heart-doctor, let's detect if your patients have arrhythmia.
-''')
+# Model selection
+model_name = st.selectbox("Select a Model", list(models.keys()))
 
-## CSV uploader
-uploaded_file = st.file_uploader(
-    "Upload your patient's ECG file below:",
-    type="csv",
-    help="Only 1 file at a time please, in .csv format"
-    )
-
-## Preview of uploaded CSV
+# File uploader
+uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
-    st.markdown("Here's a preview of the data you just uploaded:")
-    # st.write(df.T)
-    # st.write(type(df.columns)) # => pandas list d'index
-    # st.write(type(int(df.columns[10]))) # => pandas list d'index
-    # st.write(type(df.values)) # => liste de floats
-    # st.write(type(df.head(1).T[0]))
-    # st.line_chart(x=df.head(1).T[0].index, y=df.head(1).T[0])
-    st.line_chart(df)
+    st.write("Uploaded Data:", df)
 
-    ## Button to classify heartbeats
-    # st.button("Start", type="primary")
-    if st.button("Start prediction"):
-        # st.spinner('Loading...')
-        with st.spinner('Loading...'):
-            time.sleep(2)
-        st.success("This is an 'S' heartbeat.")
+    if st.button("Predict"):
+        model = models[model_name]
+
+        # Call the API
+        response = requests.post(
+            "https://huggingface.co/spaces/fabriciojm/hadt-api/predict/",
+            json={"model_name": model, "input_data": df},
+        )
+
+        if response.status_code == 200:
+            prediction = response.json()["prediction"]
+            st.write(f"Prediction using {model_name}:", prediction)
+        else:
+            st.error(f"Error: {response.json().get('detail', 'Unknown error')}")
